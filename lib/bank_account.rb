9a16dddce665f6181a0c6bdfab9752ebc69ac51a
Pav Rao <pav0107@gmail.com>
2021-08-31 14:05:02 +0100

    refactored methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,15 +7,15 @@ class BankAccount
   INITIAL_BALANCE = 0
 
   def initialize(balance = INITIAL_BALANCE)
-    @balance = ("%.2f" % balance).to_f
+    @balance = balance
     @statement = "date || credit || debit || balance\n"
   end
 
   def deposit(amount)
     @balance += amount
 
-    decimal_amount = "%.2f" % amount
-    decimal_balance = "%.2f" % @balance
+    decimal_amount = to_2_decimal_places(amount)
+    decimal_balance = to_2_decimal_places(@balance)
 
     @statement += "#{current_date} || #{decimal_amount} || || #{decimal_balance}\n"
   end
@@ -23,28 +23,21 @@ class BankAccount
   def withdraw(amount)
     @balance -= amount
 
-    decimal_amount = "%.2f" % amount
-    decimal_balance = "%.2f" % @balance
+    decimal_amount = to_2_decimal_places(amount)
+    decimal_balance = to_2_decimal_places(@balance)
 
     @statement += "#{current_date} || || #{decimal_amount} || #{decimal_balance}\n"
   end
 
   def current_date
-    if Date.today.day >= 10
-      date_day = Date.today.day
-    else
-      date_day = "0#{Date.today.day}"
-    end
-
-    if Date.today.month >= 10
-      date_month = Date.today.month
-    else
-      date_month = "0#{Date.today.month}"
-    end
-
+    date_day = "0#{Date.today.day}"[-2,2]
+    date_month = "0#{Date.today.month}"[-2,2]
     date_year = Date.today.year
-
     date = "#{date_day}/#{date_month}/#{date_year}"
+  end
+
+  def to_2_decimal_places(number)
+    "%.2f" % number
   end
 
   private
