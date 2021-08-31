21e15bffcf1bde3c7cb7eca046b7e576b381a109
Pav Rao <pav0107@gmail.com>
2021-08-31 15:39:57 +0100

    transactions now showing in the correct order

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,7 +9,9 @@ class BankAccount
 
   def initialize(balance = INITIAL_BALANCE)
     @balance = balance
-    @statement = "date || credit || debit || balance\n"
+    @statement_header = "date || credit || debit || balance\n"
+    @transactions = ""
+    @statement = @statement_header + @transactions
   end
 
   def deposit(amount)
@@ -18,7 +20,9 @@ class BankAccount
     decimal_amount = to_2_decimal_places(amount)
     decimal_balance = to_2_decimal_places(@balance)
 
-    @statement += "#{current_date} || #{decimal_amount} || || #{decimal_balance}\n"
+    @transactions = "#{current_date} || #{decimal_amount} || || #{decimal_balance}\n" + @transactions
+
+    @statement = @statement_header + @transactions
   end
 
   def withdraw(amount)
@@ -27,7 +31,9 @@ class BankAccount
     decimal_amount = to_2_decimal_places(amount)
     decimal_balance = to_2_decimal_places(@balance)
 
-    @statement += "#{current_date} || || #{decimal_amount} || #{decimal_balance}\n"
+    @transactions = "#{current_date} || || #{decimal_amount} || #{decimal_balance}\n" + @transactions
+
+    @statement = @statement_header + @transactions
   end
 
   def current_date
@@ -41,7 +47,4 @@ class BankAccount
     '%.2f' % number
   end
 
-  private
-
-  attr_reader :balance
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -17,7 +17,7 @@ describe BankAccount do
   it 'shows a withdrawl can be made' do
     subject.deposit(10)
     subject.withdraw(4)
-    new_statement = "date || credit || debit || balance\n31/08/2021 || 10.00 || || 10.00\n31/08/2021 || || 4.00 || 6.00\n"
+    new_statement = "date || credit || debit || balance\n31/08/2021 || || 4.00 || 6.00\n31/08/2021 || 10.00 || || 10.00\n"
     expect(subject.statement).to eq(new_statement)
   end
 end
