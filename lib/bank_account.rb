9f35aa6c68962f586a93f6d59b25469053b9a8c9
Pav Rao <pav0107@gmail.com>
2021-08-31 12:22:07 +0100

    initial balance of 0

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount
 
+  attr_reader :balance
+
+  INITIAL_BALANCE = 0
+
+  def initialize(balance = INITIAL_BALANCE)
+    @balance = balance
+  end
+
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,5 +1,8 @@
 require 'bank_account'
 
 describe BankAccount do
+  it 'shows a balance of 0 when I open a new bank account' do
+    expect(subject.balance).to eq 0
+  end
 
 end
